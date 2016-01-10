e8932cf130b302e7af4a039b5f756fde0621e93c
Martin Kim Dung-Pham <kim@elbedev.com>
2016-01-09 19:59:18 -0500

    Gardening

--- lib/Reader.rb
+++ lib/Reader.rb
@@ -6,10 +6,8 @@ require 'yaml'
 class Reader
 
   def initialize args
-    @cli = HighLine.new
+    setup_properties
     write_welcome_message
-    @configuration = Configuration.new
-    @aggregator = RSSAggregator.new([])
     parse_argv(args)
 
     unless @help.nil?
@@ -28,11 +26,11 @@ class Reader
     @cli.say("Your RSS feeds:")
     @cli.choose do |menu|
       menu.prompt = "Choose a feed or quit by either choosing '" + (@configuration.feeds.length + 1).to_s + "' or entering the letter 'q'."
-      @configuration.feeds.each do |feed|
+      @configuration.feeds.each do |feed| 
         nameSymbol = feed['name'].to_sym
         menu.choices(nameSymbol) {
           handle_name_url(feed['name'], feed['url'])
-        }
+        } 
       end
       menu.choice(:'quit (q)', :q) {
       }
@@ -40,6 +38,12 @@ class Reader
   end
 
   private
+  def setup_properties
+    @cli = HighLine.new
+    @configuration = Configuration.new
+    @aggregator = RSSAggregator.new([])
+  end
+
   def write_welcome_message
     file = File.open('resources/welcome_message.txt') 
     @cli.say(file.read)
