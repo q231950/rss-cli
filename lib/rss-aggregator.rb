f6f55fd9bb7a542b92c594ac82b58aeb2decade0
Martin Kim Dung-Pham <kim@elbedev.com>
2016-05-01 16:25:48 +0200

    Cleanup

--- lib/rss-aggregator.rb
+++ lib/rss-aggregator.rb
@@ -16,14 +16,14 @@ class RSSAggregator
   def read_feeds
     @feed_urls.each { |url| @feeds.push(SimpleRSS.new( open_and_read(url))) }
   end
-  
+
   private
   def open_and_read(url)
     open(url).read
   end
 
   public
-  def feed_for_url(url) 
+  def feed_for_url(url)
     index = @feed_urls.index(url)
     @feeds[index]
   end
@@ -32,16 +32,16 @@ class RSSAggregator
     @feeds = []
     read_feeds
   end
-  
+
   def add_url(url)
     if @feed_urls.any? { |obj| obj == url }
         return
     end
-    
+
     @feed_urls.push(url)
     refresh
   end
-  
+
   def feed_count
     @feeds.size
   end
@@ -63,9 +63,3 @@ class RSSAggregator
     @feeds.each_with_index { | feed, index | list_articles(index) }
   end
 end
-#
-#aggregator = RSSAggregator.new(ARGV)
-#aggregator.channel_counts
-#puts '\n'
-#aggregator.list_all
-
